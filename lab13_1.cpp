b3b4754571c651af4cd1941b859f1e12c1fdb405
kanyanat999 <kanyanat_khueankham@cmu.ac.th>
2024-12-31 22:15:36 +0700

    lab13/finish

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double arr[], int size, double result[]) {
+    double sum = 0, sum_squares = 0, product = 1, harmonic_sum = 0;
+    double max_value = arr[0], min_value = arr[0];
+    
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+        sum_squares += arr[i] * arr[i];
+        product *= arr[i];
+        harmonic_sum += 1.0 / arr[i];
+        
+        if (arr[i] > max_value) max_value = arr[i];
+        if (arr[i] < min_value) min_value = arr[i];
+    }
+    result[0] = sum / size;
+    result[1] = sqrt((sum_squares / size) - (result[0] * result[0]));
+
+    if (product > 0) {
+        result[2] = pow(product, 1.0 / size);
+    } else {
+        result[2] = 0; 
+    }
+    result[3] = size / harmonic_sum;
+    result[4] = max_value;
+    result[5] = min_value;
+}
